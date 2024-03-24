25b3f3df3b0eebcc2fc4e0dcd34b89e610fb4471
rubsuadav <rsuarezdavid@gmail.com>
2024-03-24 20:45:53 +0100

    chore: added database configuration for testing in settings.py

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,6 +90,12 @@ DATABASES = {
     }
 }
 
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

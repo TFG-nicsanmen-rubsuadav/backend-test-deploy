15f5b09d8de4abae770adeb2b5b0220402ccefa4
nicsanmen <nicsanmen@alum.us.es>
2024-03-24 20:10:13 +0100

    chore: added dummy database to ignore migration logs

--- project/settings.py
+++ project/settings.py
@@ -85,8 +85,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
 

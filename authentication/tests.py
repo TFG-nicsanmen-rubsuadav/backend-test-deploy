3ff1b17cc543f49c94b7095f6b024496563676a7
rubsuadav <rsuarezdavid@gmail.com>
2024-04-01 01:39:15 +0200

    test: added user test methods

---
+++ authentication/tests.py
@@ -0,0 +1,52 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from parameterized import parameterized
+# external imports
+import json
+import requests
+import string
+import random
+
+# local imports
+from .utils import get_test_data, generate_phone_number
+
+
+class CreateUsersAdminTest(APITestCase):
+    def create_valid_user(self):
+        fetch = requests.get('https://randomuser.me/api/')
+
+        mail = fetch.json()['results'][0]['email'].replace('example', 'gmail')
+        password = fetch.json()['results'][0]['login']['password']
+        password += random.choice(string.punctuation)
+        password += random.choice(string.ascii_uppercase)
+        password += random.choice(string.digits)
+        name = fetch.json()['results'][0]['name']['first']
+        last_name = fetch.json()['results'][0]['name']['last']
+        phone_number = generate_phone_number()
+
+        return name, last_name, mail, phone_number, password
+
+    def post_data(self, data, expected_status, expected_message):
+        response = self.client.post(
+            '/auth/create/', json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, expected_status)
+        self.assertEqual(response.data["message"], expected_message)
+
+    @parameterized.expand(get_test_data(status.HTTP_400_BAD_REQUEST))
+    def test_invalid_data(self, _, data, expected_status, expected_message):
+        self.post_data(data, expected_status, expected_message)
+
+    def test_valid_user_creation(self):
+        name, last_name, mail, phone_number, password = self.create_valid_user()
+        data = {
+            "name": name,
+            "last_name": last_name,
+            "email": mail,
+            "phone": phone_number,
+            "birth_date": "12/10/2019",
+            "password": password,
+            "rol": "customer"
+        }
+
+        self.post_data(data, status.HTTP_201_CREATED,
+                       "User created successfully")
--- authentication/utils.py
+++ authentication/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import re
+import random
 
 # local imports
 from .constants import (
@@ -78,3 +79,120 @@ def check_roles(rol: str) -> str | bool:
         if r.to_dict()["name"] == rol:
             return r.id
     return False
+
+
+# TESTS UTILS METHODS #####################################
+def generate_phone_number():
+    phone_number = ''
+    for i in range(9):
+        if i == 0:
+            phone_number += str(random.randint(6, 7))
+        else:
+            phone_number += str(random.randint(0, 9))
+    return phone_number
+
+
+def create_test_case_invalid_role(status):
+    return {"name": "test_invalid_role",
+            "data": {"rol": "autcdhorizated"}, "status": status, "result": ROLE_NOT_ALLOWED}
+
+
+def create_test_case_invalid_email_unique_email(status):
+    return {"name": "test_invalid_email_unique_email",
+            "data": {"email": "test@gmail.com", "phone": "699383767", "rol": "admin"},
+            "status": status, "result": EMAIL_ALREADY_IN_USE}
+
+
+def create_test_case_invalid_phone_unique_phone(status):
+    return {"name": "test_invalid_phone_unique_phone",
+            "data": {"email": "tt@gmail.com", "phone": "727784362", "rol": "customer"},
+            "status": status, "result": PHONE_ALREADY_IN_USE}
+
+
+def create_test_case_invalid_email(status):
+    return {"name": "test_invalid_email",
+            "data": {"email": "HSJHDSJH", "phone": "727784361", "rol": "owner"},
+            "status": status, "result": WRONG_EMAIL}
+
+
+def create_test_case_invalid_name(status):
+    return {"name": "test_invalid_name",
+            "data": {"name": "te", "email": "HSJHDSJH@gmail.com", "phone": "727784361", "rol": "owner"},
+            "status": status, "result": NAME_LENGTH}
+
+
+def create_test_case_invalid_last_name(status):
+    return {"name": "test_invalid_last_name",
+            "data": {"last_name": "te", "email": "HSJHDSJH@gmail.com", "phone": "727784361", "rol": "owner"},
+            "status": status, "result": LAST_NAME_LENGTH}
+
+
+def create_test_case_invalid_phone(status):
+    return {"name": "test_invalid_phone",
+            "data": {"email": "HSJHDSJH@gmail.com", "phone": "fg", "rol": "owner"},
+            "status": status, "result": PHONE_MALFORMED}
+
+
+def create_test_case_invalid_birth_date(status):
+    return {"name": "test_invalid_birth_date",
+            "data": {"email": "HSJHDSJH@gmail.com", "phone": "699332667", "birth_date": "dvfvtgfbv", "rol": "owner"},
+            "status": status, "result": BIRTH_DATE_FORMAT}
+
+
+def create_test_case_invalid_password_length(status):
+    return {"name": "test_invalid_password_length",
+            "data": {"email": "HSJHDSJH@gmail.com", "phone": "699332667", "birth_date": "12/10/2019", "password": "ff", "rol": "customer"},
+            "status": status, "result": PASSWORD_LENGTH}
+
+
+def create_test_case_invalid_password_lowercase(status):
+    return {"name": "test_invalid_password_lowercase",
+            "data": {"email": "HSJHDSJH@gmail.com", "phone": "699332667", "birth_date": "12/10/2019", "password": "111111111111", "rol": "customer"},
+            "status": status, "result": PASSWORD_LOWERCASE}
+
+
+def create_test_case_invalid_password_uppercase(status):
+    return {"name": "test_invalid_password_uppercase",
+            "data": {"email": "HSJHDSJH@gmail.com", "phone": "699332667", "birth_date": "12/10/2019", "password": "111111111111f", "rol": "customer"},
+            "status": status, "result": PASSWORD_UPPERCASE}
+
+
+def create_test_case_invalid_password_digit(status):
+    return {"name": "test_invalid_password_digit",
+            "data": {"email": "HSJHDSJH@gmail.com", "phone": "699332667", "birth_date": "12/10/2019", "password": "fFfFDhshdfF", "rol": "admin"},
+            "status": status, "result": PASSWORD_DIGIT}
+
+
+def create_test_case_invalid_password_special_char(status):
+    return {"name": "test_invalid_password_special_char",
+            "data": {"email": "HSJHDSJH@gmail.com", "phone": "699332667", "birth_date": "12/10/2019", "password": "111111111111fF", "rol": "admin"},
+            "status": status, "result": PASSWORD_SPECIAL_CHAR}
+
+
+def create_tests_cases(status: int):
+    return [
+        create_test_case_invalid_role(status),
+        create_test_case_invalid_email_unique_email(status),
+        create_test_case_invalid_phone_unique_phone(status),
+        create_test_case_invalid_email(status),
+        create_test_case_invalid_name(status),
+        create_test_case_invalid_last_name(status),
+        create_test_case_invalid_phone(status),
+        create_test_case_invalid_birth_date(status),
+        create_test_case_invalid_password_length(status),
+        create_test_case_invalid_password_lowercase(status),
+        create_test_case_invalid_password_uppercase(status),
+        create_test_case_invalid_password_digit(status),
+        create_test_case_invalid_password_special_char(status),
+    ]
+
+
+def get_test_data(status: int):
+    common_data = {
+        "name": "test",
+        "last_name": "test",
+        "birth_date": "10/07/1998",
+        "password": "@Password1",
+    }
+
+    return [(case["name"], {**common_data, **case["data"]}, case["status"], case["result"]) for case in create_tests_cases(status)]
